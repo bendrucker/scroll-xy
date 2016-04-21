699ad4341e2dbf1d6830c549eac2d39721a1758e
Andrew Joslin <andrewtjoslin@gmail.com>
2016-04-21 09:25:04 -0600

    Add maximumX and maximumY to result

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict'
 
 var window = require('global/window')
+var document = require('global/document')
 
 module.exports = function scrollPosition (element) {
   if (!element || element === window) {
     return {
       x: window.scrollX,
-      y: window.scrollY
+      y: window.scrollY,
+      maximumX: document.documentElement.scrollWidth - window.innerWidth,
+      maximumY: document.documentElement.scrollHeight - window.innerHeight
     }
   }
 
   return {
     x: element.scrollLeft,
-    y: element.scrollTop
+    y: element.scrollTop,
+    maximumX: element.scrollWidth - element.offsetWidth,
+    maximumY: element.scrollHeight - element.offsetHeight
   }
 }
--- test.js
+++ test.js
@@ -2,18 +2,29 @@
 
 var test = require('tape')
 var window = require('global/window')
+var document = require('global/document')
 var position = require('./')
 
 test('window', function (t) {
   window.scrollX = 0
   window.scrollY = 1
+  window.innerWidth = 80
+  window.innerHeight = 150
+
+  document.documentElement.scrollWidth = 100
+  document.documentElement.scrollHeight = 200
+
   t.deepEqual(position(window), {
     x: 0,
-    y: 1
+    y: 1,
+    maximumX: 20,
+    maximumY: 50
   })
   t.deepEqual(position(), {
     x: 0,
-    y: 1
+    y: 1,
+    maximumX: 20,
+    maximumY: 50
   })
   t.end()
 })
@@ -21,11 +32,18 @@ test('window', function (t) {
 test('element', function (t) {
   var element = {
     scrollLeft: 2,
-    scrollTop: 3
+    scrollTop: 3,
+    offsetWidth: 20,
+    offsetHeight: 80,
+    scrollWidth: 50,
+    scrollHeight: 100
+
   }
   t.deepEqual(position(element), {
     x: 2,
-    y: 3
+    y: 3,
+    maximumX: 30,
+    maximumY: 20
   })
   t.end()
 })
